af3797530cbec67c9b75f14b471770b6523da6e7
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-12-14 17:40:08 +0900

    ・新規作成。

---
+++ IOS_Xacdonald/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_Xacdonald
+//
+//  Created by xda-yoshioka on 2020/12/14.
+//  Copyright © 2020 velo.yamigiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
